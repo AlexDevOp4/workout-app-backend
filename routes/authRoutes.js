02c56df94f21d6e32247b0cf792f38f09c8433a5
Alex Ashtiany <alex.ashtiany@icloud.com>
2024-12-06 12:38:20 -0500

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,17 +1,10 @@
 import express from "express";
-<<<<<<< HEAD
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-=======
-import createUser from "../firebase/auth_signup_password.js";
-import signInUser from "../firebase/auth_signin_password.js";
-import signOutUser from "../firebase/auth_signout.js";
-import { getAuth } from "firebase/auth";
->>>>>>> 2472996 (added firebase auth)
 import firebaseApp from "../firebase/index.js";
 
 const auth = getAuth(firebaseApp);
@@ -20,11 +13,7 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   try {
-<<<<<<< HEAD
     const user = await createUserWithEmailAndPassword(auth, email, password);
-=======
-    const user = await createUser(auth, email, password);
->>>>>>> 2472996 (added firebase auth)
     res.status(201).json({ message: "User created", user });
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -34,11 +23,7 @@ router.post("/signup", async (req, res) => {
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   try {
-<<<<<<< HEAD
     const user = await signInWithEmailAndPassword(auth, email, password);
-=======
-    const user = await signInUser(auth, email, password);
->>>>>>> 2472996 (added firebase auth)
     res.status(200).json({ message: "Signed in", user });
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -47,11 +32,7 @@ router.post("/signin", async (req, res) => {
 
 router.post("/signout", async (req, res) => {
   try {
-<<<<<<< HEAD
     await signOut(auth);
-=======
-    await signOutUser(auth);
->>>>>>> 2472996 (added firebase auth)
     res.status(200).json({ message: "Signed out successfully" });
   } catch (error) {
     res.status(400).json({ error: error.message });
